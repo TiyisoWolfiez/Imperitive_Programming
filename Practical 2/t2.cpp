22174789ba554caec8abc7cd0631c87d254a9830
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-10-20 11:48:30 +0200

    Update t2.cpp

--- Practical 2/t2.cpp
+++ Practical 2/t2.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	////declare your float variable here.
+	float myPi ;
+	
+	///User Input
+	cout<<"Enter a float number:";
+	cin>>myPi;
+
+	//declare your pointer here
+float *piPointer=nullptr;
+piPointer=&myPi;
+    cout << *piPointer << endl;
+	myPi=myPi+10.3;
+	cout << *piPointer << endl;
+    return 0;
+}
 
