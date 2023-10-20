f06fa0735b0300b7df140195ca43892484927803
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-10-20 11:47:49 +0200

    Update t1.cpp

--- Practical 2/t1.cpp
+++ Practical 2/t1.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string st="programming is fun";
+	char x='X';
+	int a=75;
+	int b=29;
+	bool b1=true;
+	bool b2=false;
+	const double cnst=1337.42;
+	
+	cout<<x<<" comes after W in the alphabet"<<endl;
+	cout<<"The sum of sixty and fifteen: "<<a<<endl;
+	cout<<"The mutiplication of fourteen and a half and two: "<<b<<endl;
+	cout<<"Learning "<<st<<endl;
+	cout<<b1<<" is the value of True"<<endl;
+	cout<<b2<<" is the value of False"<<endl;
+	
+	int A;
+	int B;
+	int C;
+	int D;
+	A=14;
+	D=31;
+	B=A*D;
+	C=D/A;
+	A=A+1;
+	D=D-2;
+	cout<<A<<endl;
+	cout<<B<<endl;
+	cout<<C<<endl;
+	cout<<D<<endl;
+	cout<<B+C<<endl;
+
+	
+	return 0;
+}
 
