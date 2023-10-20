aa4332885a13cdafe94215cfa9119727f8e86bff
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-10-20 11:53:50 +0200

    Update task1.cpp

--- Practical 3/task1.cpp
+++ Practical 3/task1.cpp
@@ -1,1 +1,18 @@
-
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main() {
+	
+	double x;
+	double tValue;
+	
+	cout << "Enter a value of x: ";
+	cin>> x;
+	
+	tValue=((cos(pow(x, 2)))/(5+2*cos(x)))+((sin(pow(x, 2)))/(5*M_PI))+(tan(x)/(cos(x)+sin(x)));
+	
+	cout << "The answer is: " << setprecision(6) << tValue<< endl;
+	
+	return 0;
+}
