f4b592c672e54986b3519b1d173314eaf3bbad18
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-10-22 21:39:51 +0200

    Update loop.cpp

--- Practical 4/loop.cpp
+++ Practical 4/loop.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <sstream>
+using namespace std;
 
+int main()
+{
+	int Limit;
+	int iterator=0;
+	int counter_f=0;
+	cout<<"Enter an int: ";
+	cin>>Limit;
+	
+	while(iterator<=Limit){
+		if(iterator%2!=0){
+			counter_f+=1;
+		}
+		iterator+=1;
+		
+	}
+	cout<<"Number of Odds: "<<counter_f<<endl;
+	   
+	return 0;
+	
+}
