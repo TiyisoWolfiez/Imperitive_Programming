44a113e1e9ae07d970d8d125ad9f3acfb17d9194
TiyisoWolfiez <110611386+TiyisoWolfiez@users.noreply.github.com>
2023-10-23 14:51:09 +0200

    Update fun.cpp

--- Practical_6/fun.cpp
+++ Practical_6/fun.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <sstream>
 
+using namespace std;
+
+string input;
+int count =-1;
+
+fstream reece ("values.txt");
+string echo(string x);
+string echo(string x, int count)
+{
+	if(count%2==0)
+	{
+		cout<<x<<count<<endl;
+	}
+	else
+	{
+		cout<<count<<x<<endl;
+	}
+}
+int main()
+{
+	while(getline(reece , input))
+	{
+		count +=1;
+		cout<< echo(input)<<endl;
+		echo(input, count);
+	}
+	reece.close();
+	return 0;
+}
+string echo(string x)
+{
+	return x;
+}
